419e105a01fc5993b25155eec55b0b53ffe2a8d1
Akash Doppalapudi <akashdoppalapudi2001@gmail.com>
2022-02-27 20:52:17 +0530

    completed 30 queries

--- queries.sql
+++ queries.sql
@@ -45,4 +45,72 @@ where age<=48 and salary>=21520;
 
 /* 15 */
 select first_name, age from employee
-where first_name like "John%" and salary>=25000 and salary<=35000;
+where first_name like "John%" and salary between 25000 and 35000;
+
+/* 16 */
+select * from employee order by age desc;
+
+/* 17 */
+select * from employee order by age asc;
+
+/* 18 */
+select * from employee order by salary desc;
+
+/* 19 */
+select * from employee order by salary asc;
+
+/* 20 */
+select * from employee where age>=17 order by salary asc;
+
+/* 21 */
+select * from employee where age<=34 order by salary desc;
+
+/* 22 */
+select * from employee order by length(first_name) asc;
+
+/* 23 */
+select count(*) from employee where age>45;
+
+/* 24 */
+DELIMITER $$
+create function add_five (num int)		/* Creating a function for adding five */
+returns int
+deterministic
+begin
+declare result int;
+set result = num + 5;
+return result;
+end $$
+DELIMITER ;
+
+DELIMITER $$
+create function subtract_250 (num int)		/* Creating a function for subtracting 250 */
+returns int
+deterministic
+begin
+declare result int;
+set result = num + 5;
+return result;
+end $$
+DELIMITER ;
+
+select first_name, last_name, title, add_five(age) as age, subtract_250(salary) as salary
+from employee;
+
+/* 25 */
+select count(*) from employee where last_name like "%re" or last_name like "%ri" or last_name like "%ks";
+
+/* 26 */
+select avg(salary) from employee;
+
+/* 27 */
+select avg(salary) from employee where title="Fresher";
+
+/* 28 */
+select avg(salary) from employee where title="Programmer";
+
+/* 29 */
+select avg(salary) from employee where age between 35 and 50;
+
+/* 30 */
+select count(*) from employee where title="Fresher";
